b9a7ebb2adeb2c31c3ae0bd5c11e5e6a37832d75
Richard Quartey <53745256+Richard-Quartey@users.noreply.github.com>
2020-09-21 12:31:44 +0100

    Updated project with database file users.sql

---
+++ database/users.sql
@@ -0,0 +1,79 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 20, 2020 at 03:32 AM
+-- Server version: 10.4.13-MariaDB
+-- PHP Version: 7.4.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `nodejs-login`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `password`) VALUES
+(7, 'yuri', 'yuri@work.com', 'yuri'),
+(17, 'admin', 'admin@admin.com', 'admin'),
+(21, 'Eunju Hwang', 'ab@cd.com', 'abcd'),
+(22, 'Richard Quartey', 'niikpkp@gmail.com', 'nii100'),
+(23, 'blue', 'blue@berry.com', 'blue'),
+(25, 'ab', 'ab@dc.com', 'abc'),
+(26, 'emma genf', 'emma@genf.com', 'emma'),
+(32, 'she he', 'he@use.com', 'use'),
+(33, 'blue cheese', 'blue@cheese', 'blue'),
+(34, 'jef bez', 'jefbez@yahoo.com', 'bez'),
+(35, 'bl ado', 'bl@ado.com', 'bl'),
+(36, 'davida naa', 'naa@gmail.com', 'naa'),
+(37, 'Davida Naa Oboney Woolley', 'davida@gmail.com', 'naa');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
