28a8c4c2622cfdac7cc25eccff63eacc6d95c594
Richard Quartey <53745256+Richard-Quartey@users.noreply.github.com>
2020-09-21 11:06:15 +0100

    Update auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -14,10 +14,10 @@ const jwt = require('jsonwebtoken');
 // const bcrypt = require('bcryptjs');
 
 const db = mysql.createConnection({
-  host: process.env.DATABASE_HOST,
-  user: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'nodejs-login'
 });
 
 
