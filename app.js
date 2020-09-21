68235a271445f67404bb554b692a9de962140fa2
Richard Quartey <53745256+Richard-Quartey@users.noreply.github.com>
2020-09-21 10:44:28 +0100

    Edited the mysql login details

--- app.js
+++ app.js
@@ -37,10 +37,10 @@ var app = express();
 
 //creating the database connection
 const db = mysql.createConnection({
-  host: process.env.DATABASE_HOST,
-  user: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'nodejs-login'
 });
 
 
